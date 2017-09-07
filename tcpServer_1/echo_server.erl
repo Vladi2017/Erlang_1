250eaa11e2723536aa6ca07f3e26a62674f52f24
Vladimir Manolescu <mvmanol@yahoo.com>
2017-09-07 21:18:53 +0300

    Now the server also accept a listening port..
    Vnote: (TCP) Half-Open Connections: http://blog.stephencleary.com/2009/05/detection-of-half-open-dropped.html
    I faced with this issue.. this days..
    The server wrote "received error, closed" while whereas the client continued to wait in RecvThread in client.read(buf) API
    as if nothing happened..(see test1pn\TCPclient1Activity.java). This is not good although it is seldom..
    So, I've made echo_server a little bit verbose..

--- tcpServer_1/echo_server.erl
+++ tcpServer_1/echo_server.erl
@@ -1,10 +1,10 @@
 -module(echo_server).
--export([start/0, loop/2]).
+-export([start/1, loop/2]).
 
 % echo_server specific code
 % http://erlang.org/doc/programming_examples/bit_syntax.html
-start() ->
-	socket_server:start(?MODULE, 7000, {?MODULE, loop}).
+start(Port) ->
+	socket_server:start(?MODULE, Port, {?MODULE, loop}).
 loop(Socket,start) ->
 	gen_tcp:send(Socket, <<"keepalive",0,0,0,1,"delay",2>>),
 	loop(Socket,run);
@@ -16,13 +16,17 @@ loop(Socket,_) ->
 			session_down;
 		{ok, <<"response",Seq:4/signed-integer-unit:8,"delay",Delay:1/signed-integer-unit:8>>} ->
 			timer:sleep(Delay*10*1000),
-			gen_tcp:send(Socket,<<"keepalive",(Seq+1):4/signed-integer-unit:8,"delay",Delay:1/signed-integer-unit:8>>),
+			Result1 = gen_tcp:send(Socket,<<"keepalive",(Seq+1):4/signed-integer-unit:8,"delay",Delay:1/signed-integer-unit:8>>),
+			case Result1 of
+				ok -> ok;
+				_Else -> io:fwrite("Vl1. gen_tcp:send(..) returned: ~w~n", [Result1])
+			end,
             loop(Socket,run);
         {ok, Data} ->
 			timer:sleep(3000),
             gen_tcp:send(Socket, Data),
             loop(Socket,run);
         {error, closed} ->
-			io:format("received error, closed~n"),
+			io:format("Vl2. received error, closed~n"),
             ok
     end.
