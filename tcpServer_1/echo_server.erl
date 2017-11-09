f2e374bf72f0ac21fef5db9fe76066fde1ff98bb
Vladimir Manolescu <mvmanol@yahoo.com>
2017-11-09 17:57:23 +0200

    echo_server.erl, rel12.. changed the approach.. to avoid timer:sleep(Delay*10*1000), //Vl in loop/2.. For now we ger an exception..: 2> echo_server:start(7000). echo_server rel.12, using gen_tcp:recv/3 & non-blocking graph socket_server rel.10 reach server accept state ok 3> =INFO REPORT==== 9-Nov-2017::17:36:01 === Started TCP server. <0.36.0> 3> =INFO REPORT==== 9-Nov-2017::17:40:21 === TCP client connected. Traffic socket: #Port<0.305> =ERROR REPORT==== 9-Nov-2017::17:40:23 === Error in process <0.40.0> with exit value: {undef,[{echo_server,delayedKA,[#Port<0.305>,0,2],[]}]} =INFO REPORT==== 9-Nov-2017::17:41:21 === Vl3. session closed with {error,closed} socket_server:handle_cast/2, State = {server_state,7000,{echo_server,loop},any,#Port<0.299>} reach server accept state 3> regs(). //Vl. .. and when we compile echo_server.erl.. we get an warning that delayedKA/1 is never used..

--- tcpServer_1/echo_server.erl
+++ tcpServer_1/echo_server.erl
@@ -1,42 +1,46 @@
 -module(echo_server).
 -export([start/1, loop/2]).
 -record(vars, {bin1, iter1}).
--define(timeout, 13000).
+-define(timeout, 600000).
 
 % echo_server: delay = keepalive_interval
 % http://erlang.org/doc/programming_examples/bit_syntax.html
 start(Port) ->
-	io:format("echo_server rel.11, using gen_tcp:recv/3 & non-blocking graph~n"),
+	io:format("echo_server rel.12, using gen_tcp:recv/3 & non-blocking graph~n"),
 	{ok,Pid} = socket_server:start(?MODULE, Port, {?MODULE, loop}),
 	error_logger:info_msg("Started TCP server. ~w~n", [Pid]).
 loop(Socket,start) ->
-	ok = gen_tcp:send(Socket, <<"keepalive",0,0,0,1,"delay",2>>),
-	error_logger:info_msg("TCP client connected. ~n"),
-	loop(Socket,run);
-loop(Socket,_) ->
+	{ok,TRef1} = timer:apply_after(2000,?MODULE,delayedKA,[Socket,0,2]),
+	error_logger:info_msg("TCP client connected. Traffic socket: ~w~n", [Socket]),
+	loop(Socket,TRef1);
+loop(Socket,TRef) ->
     case gen_tcp:recv(Socket, 0, ?timeout) of % recv(Socket, Length, Timeout) -> {ok, Packet} | {error, Reason}
 		{ok, <<"server_down_cmd\n">>} ->
+			{ok, cancel} = timer:cancel(TRef),
 			server_down;
 		{ok, <<"session_down_cmd\n">>} ->
+			{ok, cancel} = timer:cancel(TRef),
 			session_down;
 		{ok, <<"response",Seq:4/signed-integer-unit:8,"delay",Delay:1/signed-integer-unit:8>>} ->
 			io:format("~w", [Seq]),
-			timer:sleep(Delay*10*1000),
-			Result1 = gen_tcp:send(Socket,<<"keepalive",(Seq+1):4/signed-integer-unit:8,"delay",Delay:1/signed-integer-unit:8>>),
-			case Result1 of
-				ok -> ok;
-				_Else -> error_logger:info_msg("Vl1. gen_tcp:send(..) returned: ~w~n", [Result1])
-			end,
-			io:format(","),
-            loop(Socket,run);
+			{ok,TRef1} = timer:apply_after(Delay*10*1000,?MODULE,delayedKA,[Socket,Seq,Delay]),
+            loop(Socket,TRef1);
         {ok, Data} ->
 			timer:sleep(3000),
             gen_tcp:send(Socket, Data),
-            loop(Socket,run);
+            loop(Socket,TRef);
         % {error, closed} ->
 			% error_logger:info_msg("Vl2. received {error, closed}~n"),
             % ok;
 		Other ->
 			% error_logger:info_msg("Vl3. received ~w~n", [Other])
+			{ok, cancel} = timer:cancel(TRef),
 			Other
     end.
+delayedKA([Socket,Seq,Delay]) ->
+	Result1 = gen_tcp:send(Socket,<<"keepalive",(Seq+1):4/signed-integer-unit:8,"delay",Delay:1/signed-integer-unit:8>>),
+	case Result1 of
+		ok -> ok;
+		_Else -> error_logger:info_msg("Vl1. gen_tcp:send(..) returned: ~w~n", [Result1])
+	end,
+	io:format(",").
